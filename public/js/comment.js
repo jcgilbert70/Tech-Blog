bade7c3a4cc8af4356671cc63b502b873e7bf4ee
Jeff <jcgilbert70@gmail.com>
2023-03-01 10:09:26 -0600

    heroku update

---
+++ public/js/comment.js
@@ -0,0 +1,10 @@
+const comment = document.querySelector('.comment')
+
+const commentHandler = () => {
+    const commentEl = document.getElementById('inputArea')
+    const inputEl = document.createElement("input")
+    inputEl.setAttribute('type', 'text')
+    commentEl.appendChild(inputEl);
+}
+
+comment.addEventListener('click', commentHandler)
---
+++ public/js/profile.js
@@ -0,0 +1,34 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blog/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete blog');
+      }
+    }
+  };
+  
+
+  
+  document
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
+
+
+const comment = document.querySelector('.comment')
+
+const commentHandler = () => {
+   const commentEl = document.getElementById('inputArea')
+   const inputEl = document.createElement("input")
+   inputEl.setAttribute('type', 'text')
+   commentEl.appendChild(inputEl);
+
+}
+
+comment.addEventListener('click', commentHandler)
